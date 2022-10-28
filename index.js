ff68eea2cd2c60c81e70cef5451088b9d1315ff8
fatcurrahman <108534271+thebegithub@users.noreply.github.com>
2022-10-28 22:13:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let conf = confirm("Anda harus berusia minimal 18 tahun untuk melihat konten ini.");
+let name = prompt("siapa nama kamu?");
+let age = prompt("berapa umur kamu?");
+
+if (age>=18) {
+alert("Welcome!");
+}
+else {
+alert("Maaf, Anda harus berusia minimal 18 tahun untuk melihat konten ini.");
+
+}
